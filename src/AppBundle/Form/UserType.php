d0b6dff61de2607ef7d66d1bc5597eea2d4bee8b
romain <julien.delsescaux@mybestpro.com>
2017-04-19 13:59:53 +0200

    add uploadFile

--- src/AppBundle/Form/UserType.php
+++ src/AppBundle/Form/UserType.php
@@ -3,6 +3,7 @@
 namespace AppBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -16,7 +17,7 @@ class UserType extends AbstractType
         $builder
             ->add('firstname')
             ->add('lastname')
-            ->add('thumbnail')
+            ->add('thumbnail', FileType::class, array('label' => 'Image de profil'))
             ->add('job')
             ->add('unit')
             ->add('description')
